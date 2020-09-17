26b1be86a439fd0b2d59c20d168876f00b6df67d
ducquang2 <quangbilun@gmail.com>
2020-09-17 18:36:27 +0700

    test

--- sonbaitap.js
+++ sonbaitap.js
@@ -2,9 +2,13 @@ function tachchu(x, y) {
     var tach = x.split(' ')
     for (var i = 0; i < x.length; i++) {
         let tong = 0;
-        if (tong <= 100) {
+        if (tong <= y) {
             tong += tach[i].length;
         } else break;
+        for (var j = 0; j < i; j++) {
+            let text = '';
+            text += ' ' + tach[j];
+        }
 }
 }
 tachchu('aaaaaa aaa aaa', 6 );
