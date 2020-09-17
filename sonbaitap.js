cd1847e84d44d32eab070939f8331301ca16c46f
ducquang2 <quangbilun@gmail.com>
2020-09-17 18:50:32 +0700

    test

--- sonbaitap.js
+++ sonbaitap.js
@@ -1,15 +1,15 @@
 function tachchu(x, y) {
     var tach = x.split(' ');
     var result = '';
+    var tong = 0;
+
     for (var i = 0; i < x.length; i++) {
-        let tong = 0;
         if (tong <= y) {
             tong += tach[i].length;
-        } else break;
-        for (var j = 0; j < i; j++) {
-            let text = '';
-            text += ' ' + tach[j];
-        } return result = text + '...';
+        } else break
 }
+    for (var j = 0; j < i; j++) {
+            result = result + ' ' + tach[j];
+        } return result + '...';
+
 }
-tachchu('aaaaaa aaa aaa', 6 );
