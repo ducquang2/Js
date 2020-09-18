65601d636cecf064e117d9c7ea4b21c98da108b6
ducquang2 <quangbilun@gmail.com>
2020-09-18 09:46:00 +0700

    Hackerrank

---
+++ Day4_1.js
@@ -0,0 +1,9 @@
+function rectangle(a,b) {
+    var peri = 0, area = 0;
+    if (a >= 1 && a <= 100 && b >= 1 && b <= 100) {
+        peri = 2*(a+b);
+        area = a*b;
+    };
+    return {peri, area};
+}
+console.log(rectangle(3,4));
