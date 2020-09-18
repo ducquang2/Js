edabf66a214f3b480e0038a6088d6e142b4808af
ducquang2 <quangbilun@gmail.com>
2020-09-18 10:01:35 +0700

    hackerrank

--- Day4_1.js
+++ Day4_1.js
@@ -1,13 +1,3 @@
-// function rectangle(a,b) {
-//     var peri = 0, area = 0;
-//     if (a >= 1 && a <= 100 && b >= 1 && b <= 100) {
-//         peri = 2*(a+b);
-//         area = a*b;
-//     };
-//     return {peri, area};
-// }
-// console.log(rectangle(3,4));
-
 function Rectangle(a, b) {
     this.length = a;
     this.width = b;
