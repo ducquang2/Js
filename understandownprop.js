63ba6b82402fbd9c06525a42b017e0a9421bdb66
ducquang2 <quangbilun@gmail.com>
2020-09-18 10:22:07 +0700

    freecodecamp

--- understandownprop.js
+++ understandownprop.js
@@ -2,5 +2,12 @@ function Bird(name) {
     this.name = name;
     this.numLegs = 2;
 }
+let duck = new Bird('Donald');
 let canary = new Bird('Tweety');
 let ownProps = [];
+for (let property in duck) {
+    if (duck.hasOwnProperty(property)) {
+        ownProps.push(property);
+    }
+}
+console.log(ownProps);
