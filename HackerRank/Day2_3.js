20879e94a4ba261afad33463d6314d709c5e37fb
ducquang2 <quangbilun@gmail.com>
2020-09-23 14:24:23 +0700

    hackerrank

---
+++ HackerRank/Day2_3.js
@@ -0,0 +1,14 @@
+function vowelsAndConsonants(s) {
+    let regex = 'aeuio';
+    var consonants = '';
+
+    for (var i = 0; i < s.length; i++) {
+        if (regex.includes(s[i])) {
+            console.log(s[i]);
+        }
+        else {
+           consonants += s[i] + '\n';
+        }
+    }
+    console.log(consonants.trim());
+}
