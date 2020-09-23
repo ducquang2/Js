b6c0f27c89e80a69c5b5e7e9d33033a633ef3ad5
ducquang2 <quangbilun@gmail.com>
2020-09-23 14:49:00 +0700

    hackerrank

--- HackerRank/Day2_3.js
+++ HackerRank/Day2_3.js
@@ -4,7 +4,7 @@ function vowelsAndConsonants(s) {
 
     for (var i = 0; i < s.length; i++) {
         if (regex.includes(s[i])) {
-            console.log(s[i]);
+             console.log(s[i]);
         }
         else {
            consonants += s[i] + '\n';
