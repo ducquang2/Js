cfb4f8ae94a0e4efddc813ada51684851165cb29
ducquang2 <quangbilun@gmail.com>
2020-09-24 14:22:14 +0700

    hackerrank

--- HackerRank/Day3_1.js
+++ HackerRank/Day3_1.js
@@ -1,3 +1,12 @@
 function getSecondLargest(nums) {
-    
+    var first = 0;
+    var second = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > first) {
+            second = first;
+            first = nums[i];
+        } else if (nums[i] > second && nums[i] < first) {
+            second = nums[i];
+        }
+    } return second;
 }
