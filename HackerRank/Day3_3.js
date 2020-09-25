d5860b121f963d0d28dc03ed376f98e46b94d1d5
ducquang2 <quangbilun@gmail.com>
2020-09-25 16:31:21 +0700

    repair

--- HackerRank/Day3_3.js
+++ HackerRank/Day3_3.js
@@ -1,8 +1,11 @@
 function isPositive(nums) {
     if (nums > 0) {
         return 'YES';
-    } if (nums === 0) {
-        throw Error('Zero Error');
-        throw Error('Negative Error'));   
+    } 
+        if (nums === 0) {
+            throw Error('Zero Error');
+    }
+        if (nums < 0) {
+            throw Error('Negative Error');   
     }
 }
