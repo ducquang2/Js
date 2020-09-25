033ae87dafa89df74e62d8462b45c3704cb5c24f
ducquang2 <quangbilun@gmail.com>
2020-09-25 19:31:49 +0700

    update

--- HackerRank/Day4_3.js
+++ HackerRank/Day4_3.js
@@ -2,4 +2,10 @@ class Polygon {
     constructor(sides) {
         this.sides = sides;
     }
+    perimeter() {
+        var sum = 0;
+        for (var i = 0; i< this.sides.length; i++) {
+            
+        }
+    }
 }
