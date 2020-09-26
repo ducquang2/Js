305774f3814504d6f6c6b26d9206b6c96eb5f9a7
ducquang2 <quangbilun@gmail.com>
2020-09-26 13:04:53 +0700

    up

--- HackerRank/Day5_2.js
+++ HackerRank/Day5_2.js
@@ -26,5 +26,5 @@ function sides(literals, ...expressions) {
     const [a, p] = expressions;
     const root = Math.sqrt((p*p) - (16*a));
     const s1 = (p + root)/4;
-    
+    const s2 = (p - root)/4;
 }
