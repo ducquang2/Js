43fd62c9e62fd6db031cb5ce968fd7d50e595c4c
ducquang2 <quangbilun@gmail.com>
2020-09-26 13:36:54 +0700

    repair

--- HackerRank/Day5_3.js
+++ HackerRank/Day5_3.js
@@ -1,17 +1,5 @@
 function modifyArray(nums) {
-    var n = nums.length;
-    var result = [];
-    const even = () => {
-        for (var i = 0; i < n; i++) {
-            result.push(nums[i] * 2)
-        } return result;
-    }
-    const odd = () => {
-        for (var i = 0; i < n; i++) {
-            result.push(nums[i] * 3)
-        } return result;
-    }
-    if (n % 2 == 0) {
-        even;
-    } else odd
+    var something = function(n) {
+        
+    } 
 }
