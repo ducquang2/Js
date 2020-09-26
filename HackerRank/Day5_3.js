3339081ca5ad32430959f8536bc15965d58ed662
ducquang2 <quangbilun@gmail.com>
2020-09-26 13:31:10 +0700

    update

--- HackerRank/Day5_3.js
+++ HackerRank/Day5_3.js
@@ -1,6 +1,15 @@
 function modifyArray(nums) {
     var n = nums.length;
+    var result = [];
+    const even = () => {
+        for (var i = 0; i < n; i++) {
+            result.push(nums[i] * 2)
+        } return result;
+    }
+    const odd = () => {
+
+    }
     if (n % 2 == 0) {
-        
+
     }
 }
