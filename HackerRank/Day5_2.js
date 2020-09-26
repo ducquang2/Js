1fea6529e92866bd77a38110100d3f93b248674f
ducquang2 <quangbilun@gmail.com>
2020-09-26 13:10:34 +0700

    up

--- HackerRank/Day5_2.js
+++ HackerRank/Day5_2.js
@@ -29,3 +29,15 @@ function sides(literals, ...expressions) {
     const s2 = (p - root)/4;
     return (s1, s2);
 }
+
+function main() {
+    let s1 = +(readLine());
+    let s2 = +(readLine());
+    
+    [s1, s2] = [s1, s2].sort();
+    
+    const [x, y] = sides`The area is: ${s1 * s2}.\nThe perimeter is: ${2 * (s1 + s2)}.`;
+    
+    console.log((s1 === x) ? s1 : -1);
+    console.log((s2 === y) ? s2 : -1);
+}
