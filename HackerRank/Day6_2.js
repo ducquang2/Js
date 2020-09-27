fdd44dff7c4660b45bd10a75d4ff08bffeb34bc6
ducquang2 <quangbilun@gmail.com>
2020-09-27 17:15:51 +0700

    update

--- HackerRank/Day6_2.js
+++ HackerRank/Day6_2.js
@@ -2,5 +2,11 @@ function getDayName(dateString) {
     let dayName = new Date(dateString).toString().slice(0, 3);
     switch (dayName) {
         case 'Sun':
+            dayName = 'Sunday';
+            break;
+        case 'Mon':
+            dayName = 'Monday';
+            break;
+        case 'Tue':
     }
 }
