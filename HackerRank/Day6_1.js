782ca7fe3bcca35c164b4498aad8cfef73ce8a47
ducquang2 <quangbilun@gmail.com>
2020-09-27 16:55:06 +0700

    update

--- HackerRank/Day6_1.js
+++ HackerRank/Day6_1.js
@@ -1,8 +1,8 @@
 function getMaxLessThanK(n, k) {
     let max = 0;
     for (var i = 1; i < n; i++) {
-        for (var j = i + 1; j < k; j ++) {
-            
+        for (var j = i + 1; j < n + 1; j ++) {
+
         }
     }
 }
