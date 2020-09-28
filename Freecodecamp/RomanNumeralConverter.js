4fa71a5798ff3203fcb81784b9431339224c7cd5
ducquang2 <quangbilun@gmail.com>
2020-09-28 10:57:57 +0700

    finaltest

--- Freecodecamp/RomanNumeralConverter.js
+++ Freecodecamp/RomanNumeralConverter.js
@@ -1,8 +1,6 @@
 function convertToRoman(num) {
     var nums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-    var romans = [
-        "M",
-        "CM",
+    var romans = ["M","CM",
         "D",
         "CD",
         "C",
