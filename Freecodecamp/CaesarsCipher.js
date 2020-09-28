5677e1addbcfcee0bf3f5f2be78a73c244d58e17
ducquang2 <quangbilun@gmail.com>
2020-09-28 11:29:54 +0700

    finaltest

--- Freecodecamp/CaesarsCipher.js
+++ Freecodecamp/CaesarsCipher.js
@@ -1,7 +1,18 @@
 function rot13(str) {
-    var rotArray = [];
-    var regex = /[A-Z]/;
-    
+    str.toUpperCase(); 
+    var abc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"];
+    var nop = ["N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var arr = str.split("");
+
+        for(var i = 0; i < arr.length; i++) {
+            for (var j = 0; j < abc.length; j ++) {
+                if (arr[i] === abc[j]) {
+                    arr[i] = nop [j];
+                } else if (arr[i] === nop[j]) {
+                    
+                }
+            }
+        }
     return str;
   }
   
