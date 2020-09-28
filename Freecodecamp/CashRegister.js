f931ccddc606793948e51f3ff36bb47d37b78150
ducquang2 <quangbilun@gmail.com>
2020-09-28 14:21:47 +0700

    finaltest

--- Freecodecamp/CashRegister.js
+++ Freecodecamp/CashRegister.js
@@ -2,5 +2,12 @@ function checkCashRegister(price, cash, cid) {
     var money = [
         {name: 'ONE HUNDRED', value: 100.0},
         {name: 'TWENTY', value: 20.0},
-    ]
+        {name: 'TEN', value: 10.0},
+        {name: 'FIVE', value: 5.0},
+        {name: 'ONE', value: 1.0},
+        {name: 'QUARTER', value: 0.25},
+        {name: 'DIME', value: 0.1},
+        {name: 'NICKEL', value: 0.05},
+        {name: 'PENNY', value: 0.01}
+    ];
 }
