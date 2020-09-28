5611f708202e24baaf97b3af4ebb12616ecabfd1
ducquang2 <quangbilun@gmail.com>
2020-09-28 14:45:03 +0700

    finaltest

--- Freecodecamp/CashRegister.js
+++ Freecodecamp/CashRegister.js
@@ -1,27 +1,73 @@
-var money = [
-    {name: 'ONE HUNDRED', value: 100.0},
-    {name: 'TWENTY', value: 20.0},
-    {name: 'TEN', value: 10.0},
-    {name: 'FIVE', value: 5.0},
-    {name: 'ONE', value: 1.0},
-    {name: 'QUARTER', value: 0.25},
-    {name: 'DIME', value: 0.1},
-    {name: 'NICKEL', value: 0.05},
-    {name: 'PENNY', value: 0.01}
-];
-
-function checkCashRegister(price, cash, cid) {
-    var output = {status: null, change: [] };
+// Create an array of objects which hold the denominations and their values
+var denom = [
+    { name: "ONE HUNDRED", val: 100.0 },
+    { name: "TWENTY", val: 20.0 },
+    { name: "TEN", val: 10.0 },
+    { name: "FIVE", val: 5.0 },
+    { name: "ONE", val: 1.0 },
+    { name: "QUARTER", val: 0.25 },
+    { name: "DIME", val: 0.1 },
+    { name: "NICKEL", val: 0.05 },
+    { name: "PENNY", val: 0.01 }
+  ];
+  
+  function checkCashRegister(price, cash, cid) {
+    var output = { status: null, change: [] };
     var change = cash - price;
-
+  
+    // Transform CID array into drawer object
     var register = cid.reduce(
-        function(acc, curr) {
-            acc.total += curr[1];
-            acc[curr[0]] = curr[1];
-            return acc;
-        },
-        {total: 0}
+      function(acc, curr) {
+        acc.total += curr[1];
+        acc[curr[0]] = curr[1];
+        return acc;
+      },
+      { total: 0 }
     );
-
-    if (register.total === change)
-}
+  
+    // Handle exact change
+    if (register.total === change) {
+      output.status = "CLOSED";
+      output.change = cid;
+      return output;
+    }
+  
+    // Handle obvious insufficient funds
+    if (register.total < change) {
+      output.status = "INSUFFICIENT_FUNDS";
+      return output;
+    }
+  
+    // Loop through the denomination array
+    var change_arr = denom.reduce(function(acc, curr) {
+      var value = 0;
+      // While there is still money of this type in the drawer
+      // And while the denomination is larger than the change remaining
+      while (register[curr.name] > 0 && change >= curr.val) {
+        change -= curr.val;
+        register[curr.name] -= curr.val;
+        value += curr.val;
+  
+        // Round change to the nearest hundreth deals with precision errors
+        change = Math.round(change * 100) / 100;
+      }
+      // Add this denomination to the output only if any was used.
+      if (value > 0) {
+        acc.push([curr.name, value]);
+      }
+      return acc; // Return the current change_arr
+    }, []); // Initial value of empty array for reduce
+  
+    // If there are no elements in change_arr or we have leftover change, return
+    // the string "Insufficient Funds"
+    if (change_arr.length < 1 || change > 0) {
+      output.status = "INSUFFICIENT_FUNDS";
+      return output;
+    }
+  
+    // Here is your change, ma'am.
+    output.status = "OPEN";
+    output.change = change_arr;
+    return output;
+  }
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
