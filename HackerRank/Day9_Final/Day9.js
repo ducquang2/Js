6b692b6b0a9c0740ac1eeefc3ee16592de52c787
Đức Quang <70498757+ducquang2@users.noreply.github.com>
2020-09-29 11:35:28 +0700

    add

--- HackerRank/Day9_Final/Day9.js
+++ HackerRank/Day9_Final/Day9.js
@@ -7,6 +7,6 @@ var buttonDiv = document.createElement('div');
 buttonDiv.id = 'btns'
 document.body.appendChild(buttonDiv);
 
-var ids = ['btn0', 'btn1', 'btnClr', 'btnSum', 'btnSub', 'btnMul','btnDiv'];
+var ids = ['btn0', 'btn1', 'btnClr', 'btnEql', 'btnSum', 'btnSub', 'btnMul','btnDiv'];
 var innerHTMLs = ['0', '1', 'C', '=', '+', '-', '*', '/'];
-var styles = []
+var styles = ['biButton', 'biButton', 'utiButton', 'utiButton', 'operButton', 'operButton', 'operButton', 'operButton']
