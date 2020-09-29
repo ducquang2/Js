5cd82941d0c3969babcbf65a697230384f0bb673
Đức Quang <70498757+ducquang2@users.noreply.github.com>
2020-09-29 11:40:36 +0700

    add

--- HackerRank/Day9_Final/Day9.js
+++ HackerRank/Day9_Final/Day9.js
@@ -9,4 +9,23 @@ document.body.appendChild(buttonDiv);
 
 var ids = ['btn0', 'btn1', 'btnClr', 'btnEql', 'btnSum', 'btnSub', 'btnMul','btnDiv'];
 var innerHTMLs = ['0', '1', 'C', '=', '+', '-', '*', '/'];
-var styles = ['biButton', 'biButton', 'utiButton', 'utiButton', 'operButton', 'operButton', 'operButton', 'operButton']
+var styles = ['biButton', 'biButton', 'utiButton', 'utiButton', 'operButton', 'operButton', 'operButton', 'operButton'];
+
+for (var i = 0; i < ids.length; i++) {
+    var button = document.createElement('button');
+    button.id = ids[i];
+    button.innerHTML = innerHTMLs[i];
+    button.className = 'button' + styles[i];
+    button.addEventListener('click', buttonClick);
+    buttonDiv.appendChild(button);
+}
+
+var opr = '';
+
+function buttonClick(e) {
+    var button = e.target || e.srcElement;
+
+    if (button.id == 'btnClr') {
+        op
+    }
+}
