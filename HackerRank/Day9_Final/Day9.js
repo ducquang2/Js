b1f3cbfef48b29b70f2d11bb315d8ee664795bc0
Đức Quang <70498757+ducquang2@users.noreply.github.com>
2020-09-29 11:30:25 +0700

    add

--- HackerRank/Day9_Final/Day9.js
+++ HackerRank/Day9_Final/Day9.js
@@ -2,3 +2,6 @@ var resulDiv = document.createElement('div');
 resultDiv.id = 'res';
 resulDiv.addEventListener('click', buttonClick);
 document.body.appendChild(resulDiv);
+
+var buttonDiv = document.createElement('div');
+ 
