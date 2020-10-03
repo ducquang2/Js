86f4135c777f7fd4a1e9551812a056a33c128184
ducquang2 <quangbilun@gmail.com>
2020-10-03 23:10:51 +0700

    update

--- Problem_Solving(HackerRank)/Compare_the_Triplets.js
+++ Problem_Solving(HackerRank)/Compare_the_Triplets.js
@@ -3,8 +3,10 @@ function compareTriplets(a, b) {
     for (var i = 0; i < a.length; i++) {
         if (a[i] > b[i]) {
             points[0]++;
-        } else if (a[i] != b[i]) {
-            
+        }
+        //because else in this line mean b[i] <= a[i], then we need != 
+        else if (a[i] != b[i]) {
+
         }
     }
 }
