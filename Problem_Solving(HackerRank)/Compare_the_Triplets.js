dcc0f5340f31a5fe6b944c75dd9502dd47a68db0
ducquang2 <quangbilun@gmail.com>
2020-10-03 23:06:27 +0700

    update

--- Problem_Solving(HackerRank)/Compare_the_Triplets.js
+++ Problem_Solving(HackerRank)/Compare_the_Triplets.js
@@ -2,7 +2,7 @@ function compareTriplets(a, b) {
     let points = [0, 0];
     for (var i = 0; i < a.length; i++) {
         if (a[i] > b[i]) {
-            
+            points[0]++;
         }
     }
 }
