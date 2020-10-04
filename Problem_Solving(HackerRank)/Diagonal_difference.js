5babea834f3ed688775ff1c3da71012e62ebd951
ducquang2 <quangbilun@gmail.com>
2020-10-04 20:37:26 +0700

    update

--- Problem_Solving(HackerRank)/Diagonal_difference.js
+++ Problem_Solving(HackerRank)/Diagonal_difference.js
@@ -1,6 +1,6 @@
 function diagonalDifference(arr) {
-    var lr = rl = 0;
-    for (var i = 0; i<n; i++) {
+    var lr = 0, rl = 0;
+    for (var i = 0; i < n; i++) {
         lr += arr[i][i];
         rl += arr[i][n-i-1];
     }  
