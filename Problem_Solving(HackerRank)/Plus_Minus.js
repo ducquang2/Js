e2f05b96bf91c3383cad7ac151d46089ceb9596b
ducquang2 <quangbilun@gmail.com>
2020-10-05 09:47:36 +0700

    update

--- Problem_Solving(HackerRank)/Plus_Minus.js
+++ Problem_Solving(HackerRank)/Plus_Minus.js
@@ -1,6 +1,16 @@
 function plusMinus(arr) {
     var pos = 0, neg = 0, zero = 0;
     for (var i = 0; i < arr.length; i++) {
-
+        switch (arr[i]) {
+            case arr[i] > 0:
+                pos++;
+                break;
+            case arr[i] < 0:
+                neg++;
+                break;
+            default: 
+                zero++;
+        }
+        return ( pos/arr.length , neg/arr.length , zero/arr.length );
     }
 }
