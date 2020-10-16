10443c8eb4d32aa6f2cde99867aa19f0e8b2e443
ducquang2 <quangbilun@gmail.com>
2020-10-16 19:19:16 +0700

    calculations

--- time-countdown.js
+++ time-countdown.js
@@ -9,5 +9,11 @@ var x = setInterval(function() {
     // Find the distance between now and the count down date
     var distance = countDownDate - now;
   
-    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+
   });
