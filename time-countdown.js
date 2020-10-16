ddc1c6c423c6eb187086487f26b4ebe708f6a4cb
ducquang2 <quangbilun@gmail.com>
2020-10-16 19:09:44 +0700

    get-date

--- time-countdown.js
+++ time-countdown.js
@@ -1,4 +1,8 @@
 // Set the date we're counting down to
 var countDownDate = new Date("Oct 18, 2021 15:37:25").getTime();
 // Update the count down every 1 second
-var x = setInterval(function() {});
+var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+  });
