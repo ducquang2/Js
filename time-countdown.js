aaf69d314557315023afa79c00c855e881dabb39
ducquang2 <quangbilun@gmail.com>
2020-10-16 19:07:15 +0700

    function

--- time-countdown.js
+++ time-countdown.js
@@ -1,2 +1,4 @@
 // Set the date we're counting down to
 var countDownDate = new Date("Oct 18, 2021 15:37:25").getTime();
+// Update the count down every 1 second
+var x = setInterval(function() {});
